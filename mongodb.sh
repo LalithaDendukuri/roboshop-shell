c3942760d5a2eaab8012d9efaaac4031ff0bfdba
Lalitha <lalitha.d339@gmail.com>
2023-07-23 16:37:17 +0530

    shell scripts

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,6 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 ##Update listen address
+sed -i 's/127.0.0.1/0.0.0.0.0/' /etc/mongd.conf
 systemctl enable mongod
 systemctl restart mongod
